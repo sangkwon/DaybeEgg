e4a3cee5a9e974cf2afaa77bb644e6aa6f0b2a33
sangkwon <sangkwon.k@gmail.com>
2013-11-23 17:18:43 +0900

    ass

--- src/com/daybe/egg/EggActivity.java
+++ src/com/daybe/egg/EggActivity.java
@@ -1,23 +1,19 @@
 
 package com.daybe.egg;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 
 public class EggActivity extends Activity {
+
+	private EggView mEggView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.egg);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.egg, menu);
-		return true;
+		mEggView = new EggView(this);
+		setContentView(mEggView);
 	}
 
 }
